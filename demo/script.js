7566f0bc618b98b639837c8da22cee9a86184859
Hanjo Meinhardt <hanjo@bunix.de>
2025-01-29 14:40:00 +0100

    update demo

--- demo/script.js
+++ demo/script.js
@@ -1,3 +1,20 @@
 const movieForm = document.getElementById("movieForm");
 const movieInput = document.getElementById("movieInput");
 const movieList = document.getElementById("movieList");
+
+// todo: implement add movie
+movieForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+});
+
+// todo: implement add movie to DOM list
+function addMovieToList(movieName) {
+}
+
+// todo: implement store movies to local storage
+function saveMovieToLocalStorage(movieName) {
+}
+
+// todo: implement loading of movies from local storage at page load
+window.addEventListener("DOMContentLoaded", () => {
+});
